a1b4f3ab06fed4a73f5948296e111f78e0005ff4
Luke0328 <curly0328@gmail.com>
2023-04-17 21:55:53 -0600

    implement front end editing of recipes and underlying state

--- client/src/MyRecipesMenu.tsx
+++ client/src/MyRecipesMenu.tsx
@@ -1,18 +1,28 @@
 import { FC, useState, useEffect } from "react";
 
 interface IRecipeBlock {
-    recipeName?: string,
+    recipeName: string,
     calories?: number,
     protein?: number,
     carbs?: number,
     fats?: number,
+    isEditing: boolean,
+    handleRecipeChange: (recipeName:string, field: string, newValue: string) => void,
+}
+
+interface IMacroRow {
+    labelName: string,
+    value?: number,
+    recipeName: string
+    isEditing: boolean,
+    handleRecipeChange: (recipeName:string, field: string, newValue: string) => void,
 }
 
 const MyRecipesMenu:FC<any> = () => {
 
     const [recipes, setRecipes] = useState<Array<IRecipeBlock>>([]);
 
-    // for testing
+    // get initial data
     useEffect( () => 
         setRecipes([
             {
@@ -21,11 +31,13 @@ const MyRecipesMenu:FC<any> = () => {
                 protein: 30,
                 carbs: 35,
                 fats: 15,
+                isEditing: false,
+                handleRecipeChange: handleRecipeChange,
             }
         ]
     ), []);
 
-    // turn list of objects to RecipeBlock components to display
+    // turn list of objects to RecipeBlock components to render
     const convertToRecipeBlocks = () => recipes.map(recipe => 
         <RecipeBlock 
             recipeName={recipe.recipeName}
@@ -33,6 +45,8 @@ const MyRecipesMenu:FC<any> = () => {
             protein={recipe.protein}
             carbs={recipe.carbs}
             fats={recipe.fats}
+            isEditing={recipe.isEditing}
+            handleRecipeChange= {handleRecipeChange}
         />
     );
 
@@ -47,10 +61,48 @@ const MyRecipesMenu:FC<any> = () => {
                 protein: 0,
                 carbs: 0,
                 fats: 0,
+                isEditing: true,
+                handleRecipeChange: handleRecipeChange,
             }
         ]
-
         setRecipes(newRecipes);
+        recipeBlockList = convertToRecipeBlocks();
+    }
+
+    function handleRecipeChange(recipeName:string, field: string, newValue: string) {
+
+        console.log(newValue);
+        let newValueInt = 0;
+        if(newValue !== "") {
+            newValueInt = parseInt(newValue);
+            if (Number.isNaN(newValueInt)) {
+                return;
+            }
+        }
+
+        let recipeNum: number = -1;
+        for (let i = 0; i < recipes.length; i++) {
+            if(recipes[i].recipeName === recipeName) {
+                recipeNum = i;
+                break;
+            }
+        }
+        
+        // create new updated recipe
+        const newRecipe = {
+            recipeName: recipes[recipeNum].recipeName,
+            calories: field === "calories" ? newValueInt: recipes[recipeNum].calories,
+            protein: field === "protein" ? newValueInt: recipes[recipeNum].protein,
+            carbs: field === "carbohydrates" ? newValueInt: recipes[recipeNum].carbs,
+            fats: field === "fats" ? newValueInt: recipes[recipeNum].fats,
+            isEditing: true,
+            handleRecipeChange: handleRecipeChange,
+        }
+
+        // update state with new recipe
+        let recipesToEdit = [...recipes];
+        recipesToEdit[recipeNum] = newRecipe;
+        setRecipes(recipesToEdit);
         recipeBlockList = convertToRecipeBlocks();
     }
 
@@ -65,7 +117,8 @@ const MyRecipesMenu:FC<any> = () => {
     );
 }
 
-const RecipeBlock:FC<IRecipeBlock> = ({ recipeName, calories, protein, carbs, fats }) => {
+const RecipeBlock:FC<IRecipeBlock> = ({ recipeName, calories, protein, carbs, fats, isEditing, handleRecipeChange }) => {
+
     return(
         <div className="flex gap-10">
             <div>
@@ -73,25 +126,74 @@ const RecipeBlock:FC<IRecipeBlock> = ({ recipeName, calories, protein, carbs, fa
             </div>
 
             <ul>
-                <li>
-                    <label>
-                        <input type="text" value={calories} className="w-10"/>
-                        calories
-                    </label>                    
+                <li key="0">
+                    <MacroRow 
+                        labelName="calories"
+                        value={calories}
+                        isEditing={isEditing}
+                        recipeName={recipeName}
+                        handleRecipeChange={handleRecipeChange}
+                    />                  
                 </li>
-                <li>
-                    {protein} protein
+                <li key="1">
+                    <MacroRow 
+                        labelName="protein"
+                        value={protein}
+                        isEditing={isEditing}
+                        recipeName={recipeName}
+                        handleRecipeChange={handleRecipeChange}
+                    />  
                 </li>
-                <li>
-                    {carbs} carbohydrates
+                <li key="2">
+                    <MacroRow 
+                        labelName="carbohydrates"
+                        value={carbs}
+                        isEditing={isEditing}
+                        recipeName={recipeName}
+                        handleRecipeChange={handleRecipeChange}
+
+                    />   
                 </li>
-                <li>
-                    {fats} fats
+                <li key="3">
+                    <MacroRow 
+                        labelName="fats"
+                        value={fats}
+                        isEditing={isEditing}
+                        recipeName={recipeName}
+                        handleRecipeChange={handleRecipeChange}
+                    />  
                 </li>
             </ul>
 
             <button>Save</button>
         </div>
+    );
+}
+
+const MacroRow:FC<IMacroRow> = ({ labelName, value, recipeName, isEditing, handleRecipeChange }) => {
+    if(isEditing) {
+        return (
+            <label>
+                <input 
+                    type="text" 
+                    value={value} 
+                    className="w-10"
+                    onChange={(e) => {handleRecipeChange(recipeName, labelName, e.target.value)}}
+                />
+                {labelName}
+            </label>  
+        );
+    }
+    return (
+        <label>
+            <input 
+                type="text" 
+                value={value} 
+                className="w-10" 
+                disabled={true} 
+            />
+            {labelName}
+        </label>  
     );
 }
 
