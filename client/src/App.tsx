18a0bb87be24465dbaf617c06bb05c57f522f4a2
Kyle <kyle.shi@colorado.edu>
2023-04-24 22:05:12 -0600

    Style login & registration pages
    Utilized useLocation hook from react-router-dom to change background of App container based off of the location.pathname. Added gradient animation to the registration/login backgrounds.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Login  from "./loginPage"
@@ -8,9 +8,19 @@ import Home from "./homePage"
 
 function App() {
   // const [page, setPage] = useState('Login');
+  const [background, setBackground] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    if(location.pathname == '/' || location.pathname == '/Register'){
+      setBackground('LoginAndRegistration');
+    } else if(location.pathname == '/Home'){
+      setBackground('bg-orange-400');
+    }
+  }, [location.pathname])
 
   return (
-    <div className="App">
+    <div className={'App ' + background }>
       {/* <Login/> */}
       <Routes>
         <Route path="Home" element={<Home />}></Route>
--- client/src/registrationPage.tsx
+++ client/src/registrationPage.tsx
@@ -40,25 +40,25 @@ const Register = () => {
 
 
     return (
-        <div className="flex flex-col w-2/5 h-2/5 rounded border-2 border-solid border-black items-center"> 
-            <h3>Start your fitness journey with TomoMacro</h3>
-            <form className="w-full flex flex-col justify-center items-center" id="registration" onSubmit={handleSubmit}>
+        <div className="flex flex-col w-2/5 h-3/5 rounded items-center bg-white shadow-xl p-5"> 
+            <h3 className="text-4xl">Register with TomoMacro!</h3>
+            <form className="w-full flex flex-col justify-center items-center h-full" id="registration" onSubmit={handleSubmit}>
                 {/* <label htmlFor="email">Email</label> */}
             
-                <div className="flex flex-col justify-center items-center w-full">
-                <div className="flex justify-between w-4/5">
-                        <input type="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First name..." id="firstName" name="firstName" className="w-full"/>
-                        <input type="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last name..." id="lastName" name="lastName" className="w-full"/>
+                <div className="flex flex-col justify-center items-center w-full h-full">
+                <div className="flex justify-between w-4/5 h-full">
+                        <input type="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First name..." id="firstName" name="firstName" className="w-full h-full mr-3 focus:outline-none border-b-2 focus:border-rose-600 text-lg"/>
+                        <input type="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last name..." id="lastName" name="lastName" className="w-full h-full ml-3 focus:outline-none border-b-2 focus:border-rose-600 text-lg"/>
                     </div>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="youremail@gmail.com" id="email" name="email" className="w-4/5"/>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="youremail@gmail.com" id="email" name="email" className="w-4/5 h-full focus:outline-none border-b-2 focus:border-rose-600 text-lg"/>
 
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id="password" name="password" className="w-4/5"/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id="password" name="password" className="w-4/5 h-full focus:outline-none border-b-2 focus:border-rose-600 text-lg"/>
                 </div>
 
-                <div className="flex flex-col w-full">
-                    <button type="submit" form="registration">Register</button>
-                    <div className="flex justify-center">Already have an account?</div>
-                    <button onClick={handleSwap}>Login</button>
+                <div className="flex flex-col w-4/5 h-full items-center">
+                    <button type="submit" form="registration" className="border-solid border-black border-2 w-full m-3 h-3/5 rounded-md text-xl bg-rose-500 text-white">Register</button>
+                    <div className="flex justify-center text-sm">Already have an account?</div>
+                    <button className="border-solid border-black border-2 w-full m-3 h-3/5 rounded-md text-xl" onClick={handleSwap}>Login</button>
                 </div>
             </form>
         </div>
